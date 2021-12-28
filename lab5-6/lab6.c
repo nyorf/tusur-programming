08d3498504e1e162f06928452d1c50b5cbd71160
Dmitry Dolgov <me@nyorf.com>
2021-12-29 00:19:22 +0700

    lab5,6 finished, executables updated

--- lab5-6/lab6.c
+++ lab5-6/lab6.c
@@ -16,7 +16,13 @@ int main() {
     printf("\nN = ");
     scanf("%d", &n);
 
-    int matrix[n][n];
+    int** matrix;
+
+    matrix=(int**)malloc(n*sizeof(int*));
+
+    for (int i = 0; i < n; i++) {
+        matrix[i]=(int*)malloc(n*sizeof(int));
+    }
 
     printf("\nГенерация и вывод матрицы:\n");
     for (int i = 0; i < n; i++) {
@@ -28,7 +34,8 @@ int main() {
     }
 
     int column_summ;
-    float vector[n];
+    float* vector;
+    vector=(float*)malloc(n*sizeof (float));
     float column_average;
     for (int j = 0; j < n; j++) {
         column_summ = 0;
@@ -44,13 +51,21 @@ int main() {
         vector[j] = column_average;
     }
 
-    int k; 
+    int k;
     float min_number = INT_MAX;
     for (k = 0; k < n; k++) {
         if (vector[k] < min_number) {
             min_number = vector[k];
         }
     }
+
     printf("\nМинимальный элемент вектора = %.2f", min_number);
+    for (int i = 0; i < n; i++) {
+        free(matrix[i]);
+    }
+
+    free(matrix);
+    free(vector);
+
     return 0;
 }
